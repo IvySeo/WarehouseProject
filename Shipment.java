10df2c32a5debaacc46057b5cd9f8688ec4dcbc8
Sshahzad14 <35118157+Sshahzad14@users.noreply.github.com>
2019-03-08 21:09:06 -0600

    Update Shipment.java

--- Shipment.java
+++ Shipment.java
@@ -3,8 +3,36 @@ import java.text.*;
 import java.io.*;
 
 public class Shipment implements Serializable{
-	
-	private static final long serialVersionUID = 1L;
-	private Manufacturer manufacturer;
-	private Product product;
+    private static final long serialVersionUID = 1L;
+    private Manufacturer manufacturer;
+    private Product product;
+    private float supplierPrice;
+    
+    public Shipment(Manufacturer manufacturer, Product product, float supplierPrice){
+        this.manufacturer = manufacturer;
+        this.product = product;
+        this.supplierPrice = supplierPrice;
+    }
+    
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+    
+    public Product getProduct() {
+        return product;
+    }
+    
+    public float getSupplierPrice() {
+        return supplierPrice;
+    }
+    
+    public void setSupplierPrice(float s) {
+        supplierPrice = s;
+    }
+
+    public boolean recieveShipment(Product product)
+    {
+        products.add(product);
+        return true;
+    }
 }
