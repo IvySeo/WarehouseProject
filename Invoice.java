7ac4c27a46f61dd5b1af4a2beecc656980b30566
Ivy Seo <ivy.heejung.seo@gmail.com>
2019-03-13 15:25:40 -0500

    Add files via upload

--- Invoice.java
+++ Invoice.java
@@ -1,12 +1,47 @@
+// author: Ivy
+// state: final
+
+// imports
 import java.util.*;
 import java.text.*;
 import java.io.*;
 
-//Not needed for stage 1 processes. So this class may be incomplete
 public class Invoice implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+
 	private Client client;
 	private Order order;
-	private float balance;
+	private float balance; 
+
+
+    publice Invoice (String client, String order, float balance){
+        this.client = client;
+        this.order = order;
+        this.balance = balance;
+    }
+
+// get and set - basic functions
+    public String getClient(){
+        return client;
+    }
+
+    public String getOrder(){
+        return order;
+    }
+
+    public void UpdateBalance(float balance){
+        this.balance += balance;
+        return balance;
+    }
+
+    public float getBalance(){
+        return balance;
+    }
+// display the information
+  public String toString() {
+    String string = "client: " + client + " order: " + order + " balance: " + balance;
+    return string;
+  }
+
 }
