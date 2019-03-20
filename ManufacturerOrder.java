ab18faebe3913e51d48f1a08933b8b2c1ca000d1
NaCoyle <47946533+NaCoyle@users.noreply.github.com>
2019-03-20 14:39:48 -0500

    +calculateTotalCost()

--- ManufacturerOrder.java
+++ ManufacturerOrder.java
@@ -6,7 +6,9 @@ public class ManufacturerOrder implements Serializable {
   private Manufacturer supplier;
   private String id;
   private static final String MFCTR_ORDER_STRING = "MO";
-  private List shippedProducts = new LinkedList();
+  private ShippedProduct shippedProducts;
+  private float totalCost;
+  
   public  ManufacturerOrder (Manufacturer supplier) {
     this.supplier = supplier;
     id = MFCTR_ORDER_STRING + (ManufacturerOrderIdServer.instance()).getId();
@@ -18,15 +20,30 @@ public class ManufacturerOrder implements Serializable {
   
   public void addProductToOrder(Product product, int qty) {
 	  ShippedProduct prdct_toAdd = new ShippedProduct(this, product, qty);
-	  shippedProducts.add(prdct_toAdd);
+	  shippedProducts = prdct_toAdd;
   }
   
-  public Iterator getProductsInOrder() {
-    return shippedProducts.iterator();
+  public float getTotalCost()
+  {
+	  return totalCost;
+	  
+  }
+  
+  public float calculateTotalCost(SuppliedProduct spl_prd)
+  {
+	  if(shippedProducts != null)
+	  {
+		  totalCost = spl_prd.getSupplyPrice() * shippedProducts.getQuantity();
+	  }
+	  return totalCost;
+  }
+  
+  public ShippedProduct getProductsInOrder() {
+    return shippedProducts;
   }
   
   public String toString(){
-	  String string = "Manufacturer Order ID: " + id + " | Supplier: " + supplier.toString();
+	  String string = "Manufacturer Order ID: " + id + " | Supplier: " + supplier.toString() + "| Total Cost: $" + totalCost;
 	  
 	  return string;
   }
